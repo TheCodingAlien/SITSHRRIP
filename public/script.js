71e448611d8e5c8ee3e584a0381f01d81077a2de
Angelo Vartuca <angelo.v@hotmail.co.uk>
2018-05-09 15:30:35 +0100

    js updates for agile scripting
    updates to latest summary for javacript

--- public/script.js
+++ public/script.js
@@ -1,47 +1,32 @@
-//question function av
-var choices = ["answerChoice", "answerChoice2", "answerChoice3", "answerChoice4"]; 
-var choice0 = document.getElementById("answerChoice1");
-var choice1 = document.getElementById("answerChoice2");
-var choice2 = document.getElementById("answerChoice3");   
-var choice3 = document.getElementById("answerChoice4");
-// av end
-var currentQuestion = 1
-function findChoice(question) {
-     var index = -1;
+var questions = [ 
+  {
+    text: "How do you open sits", // one string
+    options: ["one click" , "two click" , "three click"]  , //three strings                       // - object 1
+    chosen: null      
+  },
+  {
+    text: "How do you close a screen in sits", // one string
+    options: ["click the X" , "file > quit" , "either"]  , //three strings                             //-object 2
+    chosen: null      
+  }
+];
 
-     // look through the choices
-     choices.forEach( function(choice, arrayIndex) {
-         if (choice.question === question) {
-             index = arrayIndex;
-         }
-     } );
+// ================================================================================================================
 
-     // return the course index if found.
-     return index;
- }
-function nextQuest() {
-  var choice = jQuery('input[name="choice"]:checked');
-//choose choice
-var choiceIndex= findChoice(currentQuestion);
-if (choiceIndex===-1){
-  choices.push({question: currentQuestion, answer: choice.val()});
-} else {
-  choices[choiceIndex].answer=choice.val();
+// get quiz space
+var quiz = document.getElementById("quiz");  // function to be called. documentgetElement calls the function from the DOM. writing functuion instead of var defines it.
+
+function questionToHtml(question, index){  //2 parameters
+  var html = `<h2>${question.text}</h2>`; // string literal. it puts the value 'question.text' inside there. takes 'text from config above etc'
+  html = question.options.reduce(optionToHtml, html); //
+  return html;
 }
-  console.log(choices);
+function optionToHtml(html, option, index){  //2 parameters // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce - accumulator is htmk, therefore had to be changed from the 3rd inline to the first - as example suggests
+  html += `<div class="option"><input type="radio" id="choice${index}" name="choice" value=${index}><label for="choice${index}">${option}</label></div>`; // string literal. it puts the value 'question.text' inside there
+//uses the options to give us all the options in the config described above //uses the index so it separates all the bits in the page
+  return html;
 }
-//question function av
-function findChoice(question) {
-document.getElementById("next").style.display="none"
-displayQuest++;
-
-var quest = document.getElementById("current")==true;
-quest.innerHTML = displayQuest;
-} else  {
-	var quest= document.getElementById("noncurrent")==false; nextQuestion(displayQuest + 1);
-	}
-
-function nextQuestion(){
-document.getElementById 
-}
-//av end
+//turn question into HTML
+var current = questionToHtml (questions[0],0);
+quiz.innerHTML = current ;
+//display HTML
