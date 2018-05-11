c9b46e081a7b0647fec56b3de8244ab2efe265b4
Angelo Vartuca <angelo.v@hotmail.co.uk>
2018-05-11 10:40:34 +0100

    updates to javascript & jquery
    -specify option change functions

--- public/script.js
+++ public/script.js
@@ -13,12 +13,12 @@ var questions = [
 
 // ================================================================================================================
 
-// get quiz space
-var quiz = document.getElementById("quiz");  // function to be called. documentgetElement calls the function from the DOM. writing functuion instead of var defines it.
 
 function questionToHtml(question, index){  //2 parameters
   var html = `<h2>${question.text}</h2>`; // string literal. it puts the value 'question.text' inside there. takes 'text from config above etc'
-  html = question.options.reduce(optionToHtml, html); //
+  html += `<div data-question-index="${index}">`;
+  html += question.options.reduce(optionToHtml, ''); //
+  html += '</div>';  
   return html;
 }
 function optionToHtml(html, option, index){  //2 parameters // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce - accumulator is htmk, therefore had to be changed from the 3rd inline to the first - as example suggests
@@ -26,7 +26,33 @@ function optionToHtml(html, option, index){  //2 parameters // https://developer
 //uses the options to give us all the options in the config described above //uses the index so it separates all the bits in the page
   return html;
 }
-//turn question into HTML
-var current = questionToHtml (questions[0],0);
-quiz.innerHTML = current ;
+function writeQuestion (number, quizElement){ //turn question into HTML
+  var current = questionToHtml (questions[number],number);
+  quizElement.innerHTML = current ;
+  addQuestionListener (quizElement);
+}
+function addQuestionListener (quizElement){
+  var inputs = quizElement.querySelectorAll('input');
+  inputs.forEach (addOptionListener); //https://developer.mozilla.org/en-US/docs/Web/API/NodeList/forEach
+}
+function addOptionListener (input){
+  input.addEventListener("change", optionChange);
+
+}
+function optionChange (e){
+  var currentQuestion = this.parentElement.parentElement.getAttribute("data-question-index");
+  var currentOption = this.value ;
+  if (this.checked) {
+    questions[currentQuestion].chosen = currentOption;
+  }
+}
+
+
+// ================================================================================================================
+// get quiz space
+var quizElement = document.getElementById("quiz");  // function to be called. documentgetElement calls the function from the DOM. writing functuion instead of var defines it.
+var currentQuestion = 1;
+
+
 //display HTML
+writeQuestion (currentQuestion, quizElement);
